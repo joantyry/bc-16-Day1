bb8b805bb45d3f7bf4177deb12dd72a675a36322
joantyry <tyry7979@gmail.com>
2017-03-07 09:02:01 +0300

    first commit

--- carclass.py
+++ carclass.py
@@ -1,33 +1,87 @@
+
 class Car(object):
-  speed = 0
+    def __init__(self, car_name='General', car_model='GM',
+                 car_type='saloon', doors = 4 ,wheels = 4 ):
+        self.car_name = car_name
+        self.car_model = car_model
+        self.doors = doors
+        self.car_type = car_type
+        self.wheels = wheels
+        self.speed = 0
 
-  def __init__(self, name='General', model='GM', vehicle_type=None):
-    self.name = name
-    self.model = model
-    self.vehicle_type = vehicle_type
+    @property
+    def name(self):
+        return self.car_name
 
+    @property
+    def model(self):
+        return self.car_model
 
-    if self.name in ['Porshe', 'Koenigsegg']:
-      self.num_of_doors = 2
-    else:
-      self.num_of_doors = 4
+    @property
+    def num_of_doors(self):
+        if self.car_name in ['Koenigsegg', 'Porshe']:
+            self.doors = 2
+           
+        return self.doors
 
-    if self.vehicle_type == 'trailer':
-      self.num_of_wheels = 8
-    else:
-      self.num_of_wheels = 4
+    @property
+    def num_of_wheels(self):
+        if self.car_type == 'trailer':
+            self.wheels = 8
+            
+        return self.wheels
 
+    def is_saloon(self):
+        if self.car_type == 'saloon':
+            return True
+        return False
 
-  def is_saloon(self):
-    if self.vehicle_type is not 'trailer':
-        self.vehicle_type == 'saloon'
-        return True
-    return False
+    @property
+    def num_of_wheels(self):
+        if self.car_type == 'trailer':
+            self.wheels = 8
+            
+        return self.wheels
 
-  def drive(self, moving_speed):
-    if moving_speed == 3:
-      Car.speed = 1000
-    elif moving_speed == 7:
-      Car.speed = 77
+    @property
+    def type(self):
+        return self.car_type
 
-    return self
+    def drive(self,  drive_number):
+        if self.car_type == 'trailer':
+            if drive_number in range(1, 8):
+                self.speed = 11 * drive_number
+                
+            return self
+        else:
+            if drive_number in range(1, 4):
+                self.speed = 997 + drive_number
+                
+            return self
+
+def main():
+    man = Car('MAN', 'Truck', 'trailer')
+    moving_man = man.drive(7)
+    moving_man_instance = isinstance(moving_man, Car)
+    moving_man_type = type(moving_man) is Car
+    print(moving_man_instance, moving_man_type, moving_man.speed, man.speed)
+
+    man = Car('Mercedes', 'SLR500')
+    parked_speed = man.speed
+    moving_speed = man.drive(3).speed
+
+    print(parked_speed, moving_speed)
+
+    honda = Car('Honda')
+    print(type(honda) is Car)
+
+    man = Car('MAN', 'Truck', 'trailer')
+    koenigsegg = Car('Koenigsegg', 'Agera R')
+    print(man.num_of_wheels, koenigsegg.num_of_wheels)
+
+    opel = Car('Opel', 'Omega 3')
+    porshe = Car('Porshe', '911 Turbo')
+    print(opel.num_of_doors,porshe.num_of_doors,Car('Koenigsegg', 'Agera R').num_of_doors)
+
+    toyota = Car('Toyota', 'Corolla')
+    print(toyota.name, toyota.model)    
